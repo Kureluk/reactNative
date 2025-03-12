cd2f9209ef0248ef793beb5093205eccf720e179
Misha Kureluk <mishakureluk@gmail.com>
2025-03-12 22:31:18 +0200

    Gallary

--- screens/About.tsx
+++ screens/About.tsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
-import { AboutScreenProps, HomeScreenProps } from './types'
+import React from 'react';
+import { View, Image, FlatList, StyleSheet, Button } from 'react-native';
+import Stars from '../images/stars.jpg';
+import Img from '../images/2.jpg';
+import Van from '../images/Vincent.jpg';
+import { HomeScreenProps } from './types'
 
-const Home = ({ navigation }: AboutScreenProps) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>About Screen</Text>
-            <Button
-                title='Go Back Home'
-                onPress={() =>
-                    navigation.popToTop()
-                } />
-        </View>
-    )
-}
+const paintings = [
+  { id: '1', image: Stars }, 
+  { id: '2', image: Img }, 
+  { id: '3', image: Van }, 
+  { id: '4', image: Stars }, 
+];
 
-export default Home
+const About = ({ navigation }: HomeScreenProps) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={paintings}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <Image source={item.image} style={styles.image} />
+        )}
+      />
+    <Button title="Назад" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
+
+export default About;
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: 'center',
-        marginTop: 20,
-        gap: 10
-    },
-    text: {
-        color: 'black',
-        fontSize: 18,
-        textAlign: 'center'
-    },
-})
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: '1%',
+    borderRadius: 10,
+  },
+});
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 import { HomeScreenProps } from './types'
+import { Image } from 'react-native';
+import VanGoghImage from '../images/Vincent.jpg';
 
 const Home = ({ navigation }: HomeScreenProps) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Home Screen</Text>
-            <Button
-                title='Go to Forms'
-                onPress={() =>
-                    navigation.navigate("Forms", { initialEmail: "example@ukr.net" })
-                } />
-        </View>
+        <Text style={styles.title}>Вінсент ван Гог</Text>
+        <Image source={VanGoghImage} style={styles.image} />
+        <Text style={styles.description}>
+          Вінсент ван Гог — відомий нідерландський художник, один із найвпливовіших представників постімпресіонізму.
+        </Text>
+        <Button title="Галерея" onPress={() => navigation.navigate('About')} />
+      </View>
     )
 }
 
@@ -19,15 +21,26 @@ export default Home
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex',
-        justifyContent: "center",
-        alignItems: 'center',
-        marginTop: 20,
-        gap: 10
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 20,
+      backgroundColor: '#f5f5f5',
     },
-    text: {
-        color: 'black',
-        fontSize: 18,
-        textAlign: 'center'
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 10,
     },
-})
+    image: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      marginBottom: 15,
+    },
+    description: {
+      fontSize: 16,
+      textAlign: 'center',
+      marginBottom: 20,
+    },
+  });
